e3fa2a7a3ba85a4841024a6a779d9d2d04ea2a77
Colbin Hubbard <colbinjustin@gmail.com>
2022-08-01 22:06:16 -0500

    class work

---
+++ server/app.js
@@ -0,0 +1,62 @@
+// 'Import' the Express module instead of http
+const express = require("express");
+const pizzas = require("./routers/pizzas");
+// Initialize the Express application
+const app = express();
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+mongoose.connect(process.env.MONGODB);
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
+  response.send(JSON.stringify({ message: "Service healthy" }));
+});
+
+app.get("/echo/:content", (request, response) => {
+  // express adds a "params" Object to requests
+  const content = request.params.content;
+  // handle GET request for post with an id of "id"
+  response.send(JSON.stringify({ echoed: content }));
+});
+
+app.use("/pizzas", pizzas);
+
+// Tell the Express app to start listening
+// Let the humans know I am running and listening on 4040
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
